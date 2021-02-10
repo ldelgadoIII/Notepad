b37566b112f99cc6c3222cffa22a8eda70d86f31
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-10 14:34:53 -0600

    created server.js file. amended start script

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,3 @@
-// Dependencies
-
-const express = require("express");
-const path = require("path");
-
-// Sets up the Express App
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
