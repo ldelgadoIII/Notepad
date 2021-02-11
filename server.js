7557c06a61af10e4b819db1696ee04d2bc5243d0
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-11 16:24:43 -0600

    added ability to read json file contents. added get request to db contents

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("./db/db.json");
+let db = JSON.parse(rawdata);
 
 // Sets up the Express App
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
@@ -13,19 +17,28 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// GET /notes should return the notes.html file
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public", "notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// GET /notes should return the notes.html file
 // GET * should return the index.html file.
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => res.json(db));
+
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+});
+
 // find a way to give each note a unique id when it's saved
 
 // app.get("*", (req, res) => {
 //   res.send("Charlie is the cooliest");
 // });
-// app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
