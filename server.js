b509dd5ea08f614be6afdc82fc8343dd989f1e5d
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-10 19:24:58 -0600

    fixed link path to stylsheet. added pseudo code for server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// GET /notes should return the notes.html file
+// GET * should return the index.html file.
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+// find a way to give each note a unique id when it's saved
+
 app.get("*", (req, res) => {
   res.send("Charlie is the cooliest");
 });
