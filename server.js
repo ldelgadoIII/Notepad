41d80d220c990fe813e569c404817e60e83c1e85
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-11 21:17:02 -0600

    code refactor

--- server.js
+++ server.js
@@ -32,15 +32,13 @@ app.post("/api/notes", (req, res) => {
   res.json(newNote);
 });
 
-// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// delete note
 app.delete("/api/notes/:id", (req, res) => {
   let note = req.params.id;
 
   db.forEach((element) => {
     if (element.id === note) {
-      db = db.filter((el) => {
-        el !== element;
-      });
+      db = db.filter((el) => el !== element);
       res.send(db);
     }
   });
